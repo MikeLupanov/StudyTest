cab017b4f5f7d95047ba3a57b32f78255c2befd9
winwood <winwood@mail.ru>
2024-10-20 12:30:21 +0300

    реорганизация каталогов

--- Vigenere/vigenere.cpp
+++ Vigenere/vigenere.cpp
@@ -22,8 +22,9 @@ auto make_alpha_num
 {
     [](const std::string & s) {
         std::map <char,int> r;
-        for(unsigned i=0; i<s.size(); i++)
+        for(unsigned i=0; i<s.size(); i++) {
             r[s[i]] = i;
+        }
         return r;
     }
 };
@@ -33,20 +34,24 @@ const std::map <char,int> Vigenere::alpha_num {make_alpha_num(Vigenere::alpha)};
 //конструктор для установки ключа - реализация
 Vigenere::Vigenere(const std::string& skey)
 {
-    if (skey.size() == 0)
-        throw std::invalid_argument("Empty key");     // исключение, пустой ключ
-    if (std::find_if_not(skey.begin(),skey.end(),isalpha) != skey.end())
-        throw std::invalid_argument("Non-alpha symbols in key");  // исключение, если не буквы
+    if (skey.size() == 0) {
+        throw std::invalid_argument("Empty key");    // исключение, пустой ключ
+    }
+    if (std::find_if_not(skey.begin(),skey.end(),isalpha) != skey.end()) {
+        throw std::invalid_argument("Non-alpha symbols in key");    // исключение, если не буквы
+    }
     key = convert(skey);
-    if (std::count(key.begin(), key.end(), 0) >= key.size()/2.0)
-        throw std::invalid_argument("Weak key");     // исключение, если слабый ключ
+    if (std::count(key.begin(), key.end(), 0) >= key.size()/2.0) {
+        throw std::invalid_argument("Weak key");    // исключение, если слабый ключ
+    }
 }
 
 //зашифрование - реализация
 std::string Vigenere::encrypt(const std::string& open_text)
 {
-    if (std::find_if_not(open_text.begin(),open_text.end(),isalpha) != open_text.end())
-        throw std::invalid_argument("Non-alpha symbols in message"); // исключение, если не буквы
+    if (std::find_if_not(open_text.begin(),open_text.end(),isalpha) != open_text.end()) {
+        throw std::invalid_argument("Non-alpha symbols in message");    // исключение, если не буквы
+    }
     std::vector<int> work = convert(open_text);
     for(unsigned i=0; i < work.size(); i++) {
         work[i] = (work[i] + key[i % key.size()]) % alpha_num.size();
@@ -57,8 +62,9 @@ std::string Vigenere::encrypt(const std::string& open_text)
 //расшифрование - реализация
 std::string Vigenere::decrypt(const std::string& cipher_text)
 {
-    if (std::find_if_not(cipher_text.begin(),cipher_text.end(),isalpha) != cipher_text.end())
-        throw std::invalid_argument("Non-alpha symbols in message");     // исключение, если не буквы
+    if (std::find_if_not(cipher_text.begin(),cipher_text.end(),isalpha) != cipher_text.end()) {
+        throw std::invalid_argument("Non-alpha symbols in message");    // исключение, если не буквы
+    }
     std::vector<int> work = convert(cipher_text);
     for(unsigned i=0; i < work.size(); i++) {
         work[i] = (work[i] + alpha_num.size() - key[i % key.size()]) % alpha_num.size();
